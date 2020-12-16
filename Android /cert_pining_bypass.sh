962a3871be73b34cfd86c4f366ed30ccd0f43933
Fly <peiyu94@gatech.edu>
2020-12-16 13:06:05 -0500

    Update cert_pining_bypass.sh
    update to 14.1.3

--- Android /cert_pining_bypass.sh
+++ Android /cert_pining_bypass.sh
@@ -10,14 +10,14 @@ if [ "$1" != "" ]; then
 
 	#Download frida-server, push to the device
 	wget https://github.com/frida/frida/releases/download/14.1.3/frida-server-14.1.3-android-arm64.xz
-	unxz frida-server-12.8.13-android-arm64.xz
-	adb push frida-server-12.8.13-android-arm64 /sdcard/frida-server-12.8.13-android-arm64
-	adb shell 'su -c mv /sdcard/frida-server-12.8.13-android-arm64 /data/local/tmp/frida-server-12.8.13-android-arm64'
-	adb shell 'su -c chmod +x /data/local/tmp/frida-server-12.8.13-android-arm64'
+	unxz frida-server-14.1.3-android-arm64.xz
+	adb push frida-server-14.1.3-android-arm64 /sdcard/frida-server-14.1.3-android-arm64
+	adb shell 'su -c mv /sdcard/frida-server-14.1.3-android-arm64 /data/local/tmp/frida-server-14.1.3-android-arm64'
+	adb shell 'su -c chmod +x /data/local/tmp/frida-server-14.1.3-android-arm64'
 
-	rm frida-server-12.8.13-android-arm64*
+	rm frida-server-14.1.3-android-arm64*
 
-	adb shell 'su -c /data/local/tmp/frida-server-12.8.13-android-arm64 &' || echo 'frida server already running'
+	adb shell 'su -c /data/local/tmp/frida-server-14.1.3-android-arm64 &' || echo 'frida server already running'
 
 	#bypass cert pining with frida
 	frida --codeshare pcipolloni/universal-android-ssl-pinning-bypass-with-frida -U -f $1 --no-pause
